8831c63c816ea1823ac0504c04759aa65cd9adc8
ZoranCelebic <92189212+ZoranCelebic@users.noreply.github.com>
2021-11-22 01:45:09 +0000

    Update

--- Zoran Project.py
+++ Zoran Project.py
@@ -97,98 +97,98 @@ df["great_game"] = np.select(conditions, val)
 
 
 great_game = df[df["great_game"].str.contains("yes")]
+
 #ploting point plot of average meta_score per company
 
-#sns.set_style("darkgrid")
-#g1 = sns.catplot(x="Company", y="meta_score", data=df, kind="box", ci=None, palette=["r", "b", "g"])
-#g1.fig.suptitle("Meta score distribution per company", y=0.99, color="red",)
-#g1.set(xlabel="Company", ylabel = "Meta Score")
-#g1.set_xticklabels(color="darkblue")
-#g1.set_yticklabels(color="darkblue")
-#plt.savefig("average_meta_score_per_company.jpg")
-#plt.show()
+sns.set_style("darkgrid")
+g1 = sns.catplot(x="Company", y="meta_score", data=df, kind="box", ci=None, palette=["r", "b", "g"])
+g1.fig.suptitle("Meta score distribution per company", y=0.99, color="red",)
+g1.set(xlabel="Company", ylabel = "Meta Score")
+g1.set_xticklabels(color="darkblue")
+g1.set_yticklabels(color="darkblue")
+plt.savefig("average_meta_score_per_company.jpg")
+plt.show()
 
 #ploting line plot of average meta_score per year per company
 
-#sns.set_style("ticks")
-#g2 = sns.lineplot(x="release_year", y="meta_score", data=df, hue="Company", ci=None, palette=["r", "b", "g"], alpha=0.7, style="Company)
-#g2.set_title("Average meta score 1995.-2021.", y=1.05, color="red",)
-#g2.set(xlabel="Release year", ylabel="Average meta score")
-#g2.tick_params(axis="x", colors="darkblue")
-#g2.tick_params(axis="y", colors="darkblue")
-#g2.set(xticks=df.release_year)
-#plt.xticks(rotation=90)
-#plt.subplots_adjust(bottom=0.15)
-#plt.savefig("average_meta_score_per_company_per_year.jpg")
-#plt.show()
+sns.set_style("ticks")
+g2 = sns.lineplot(x="release_year", y="meta_score", data=df, hue="Company", ci=None, palette=["r", "b", "g"], alpha=0.7, style="Company")
+g2.set_title("Average meta score per company 1995.-2021.", y=1.05, color="red",)
+g2.set(xlabel="Release year", ylabel="Average meta score")
+g2.tick_params(axis="x", colors="darkblue")
+g2.tick_params(axis="y", colors="darkblue")
+g2.set(xticks=df.release_year)
+plt.xticks(rotation=90)
+plt.subplots_adjust(bottom=0.15)
+plt.savefig("average_meta_score_per_company_per_year.jpg")
+plt.show()
 
 #plot point plot of mean user review per company
 
-#sns.set_style("darkgrid")
-#g3 = sns.catplot(x="Company", y="user_review", data=df, kind="box", ci=None, palette=["r", "b", "g"], whis=[5, 95])
-#g3.fig.suptitle("User review distribution per company", color="red", y=0.98)
-#g3.set(xlabel="Company", ylabel="User review")
-#g3.set_xticklabels(color="darkblue")
-#g3.set_yticklabels(color="darkblue")
-#plt.savefig("average_user_review_per_company.jpg")
-#plt.show()
+sns.set_style("darkgrid")
+g3 = sns.catplot(x="Company", y="user_review", data=df, kind="box", ci=None, palette=["r", "b", "g"], whis=[5, 95])
+g3.fig.suptitle("User review distribution per company", color="red", y=0.98)
+g3.set(xlabel="Company", ylabel="User review")
+g3.set_xticklabels(color="darkblue")
+g3.set_yticklabels(color="darkblue")
+plt.savefig("average_user_review_per_company.jpg")
+plt.show()
 
 #ploting line plot of mean meata_score per year per company
 
-#sns.set_style("ticks)
-#g4 = sns.lineplot(x="release_year", y="user_review", data=df, hue="Company", ci=None, palette=["r", "b", "g"], alpha=0.7, style="Company")
-#g4.set_title("Average user score 1995.-2021.", y=1.05, color="red",)
-#g4.set(xlabel="Release year", ylabel="Average user review")
-#g4.tick_params(axis="x", colors="darkblue")
-#g4.tick_params(axis="y", colors="darkblue")
-#g4.set(xticks=df.release_year)
-#plt.xticks(rotation=90)
-#plt.subplots_adjust(bottom=0.15)
-#plt.savefig("average_user_review_per_company_per_year.jpg")
-#plt.show()
+sns.set_style("ticks")
+g4 = sns.lineplot(x="release_year", y="user_review", data=df, hue="Company", ci=None, palette=["r", "b", "g"], alpha=0.7, style="Company")
+g4.set_title("Average user score per company 1995.-2021.", y=1.05, color="red",)
+g4.set(xlabel="Release year", ylabel="Average user review")
+g4.tick_params(axis="x", colors="darkblue")
+g4.tick_params(axis="y", colors="darkblue")
+g4.set(xticks=df.release_year)
+plt.xticks(rotation=90)
+plt.subplots_adjust(bottom=0.15)
+plt.savefig("average_user_review_per_company_per_year.jpg")
+plt.show()
 
 #plot mean meta score and user review per platform
 
 sns.set_style("darkgrid")
-#g5 = sns.catplot(data=df,x="platform", y="meta_score", kind="point", hue="Company", ci=None, palette=["r", "b", "g"])
-#g5.fig.suptitle("Meta score per platform", color="red", y=0.98)
-#g5.set(xlabel="Company", ylabel="Meta Score")
-#g5.set_xticklabels(color="darkblue")
-#g5.set_yticklabels(color="darkblue")
-#plt.xticks(rotation=90)
-#plt.subplots_adjust(bottom=0.35)
-#plt.savefig("Meta score per platform.jpg")
-#plt.show()
+g5 = sns.catplot(data=df,x="platform", y="meta_score", kind="point", hue="Company", ci=None, palette=["r", "b", "g"])
+g5.fig.suptitle("Meta score per platform", color="red", y=0.98)
+g5.set(xlabel="Company", ylabel="Meta Score")
+g5.set_xticklabels(color="darkblue")
+g5.set_yticklabels(color="darkblue")
+plt.xticks(rotation=90)
+plt.subplots_adjust(bottom=0.35)
+plt.savefig("Meta score per platform.jpg")
+plt.show()
 
-#g6 = sns.catplot(data=df,x="platform", y="user_review", kind="point", hue="Company", ci=None, palette=["r", "b", "g"])
-#g6.fig.suptitle("User score per platform", color="red", y=0.98)
-#g6.set(xlabel="Company", ylabel="User score")
-#g6.set_xticklabels(color="darkblue")
-#g6.set_yticklabels(color="darkblue")
-#plt.xticks(rotation=90)
-#plt.subplots_adjust(bottom=0.35)
-#plt.savefig("User score per platform.jpg")
-#plt.show()
+g6 = sns.catplot(data=df,x="platform", y="user_review", kind="point", hue="Company", ci=None, palette=["r", "b", "g"])
+g6.fig.suptitle("User score per platform", color="red", y=0.98)
+g6.set(xlabel="Company", ylabel="User score")
+g6.set_xticklabels(color="darkblue")
+g6.set_yticklabels(color="darkblue")
+plt.xticks(rotation=90)
+plt.subplots_adjust(bottom=0.35)
+plt.savefig("User score per platform.jpg")
+plt.show()
 
 #plot median meta_user
 
 from numpy import median
-#g7 = sns.catplot(x="Company", y="meta_user", data=df, kind="point", ci=None, estimator=median, palette=["r", "b", "g"])
-#g7.fig.suptitle("Median Meta_User/2 score per company", y=0.99, color="red",)
-#g7.set(xlabel="Company", ylabel = "Meta_user score")
-#g7.set_xticklabels(color="darkblue")
-#g7.set_yticklabels(color="darkblue")
-#plt.savefig("average_meta_user_per_company.jpg")
-#plt.show()
+g7 = sns.catplot(x="Company", y="meta_user", data=df, kind="point", ci=None, estimator=median, palette=["r", "b", "g"])
+g7.fig.suptitle("Median Meta_User/2 score per company", y=0.99, color="red",)
+g7.set(xlabel="Company", ylabel = "Meta_user score")
+g7.set_xticklabels(color="darkblue")#
+g7.set_yticklabels(color="darkblue")
+plt.savefig("average_meta_user_per_company.jpg")
+plt.show()
 
 #plot corelation heatmap
 
-#g8 = sns.heatmap(df.corr(), annot=True, fmt=".2g", cmap="coolwarm",
-                # xticklabels=["Meta Score", "User review", "Release year", "Meta User"],
-                 #yticklabels=["Meta Score", "User review", "Release year", "Meta User"])
-#g8.set_title("Corelation heatmap")
-#plt.savefig("Corelation heatmap.jpg")
-#plt.show()
+g8 = sns.heatmap(df.corr(), annot=True, fmt=".2g", cmap="coolwarm",
+                 xticklabels=["Meta Score", "User review", "Release year", "Meta User"],
+                 yticklabels=["Meta Score", "User review", "Release year", "Meta User"])
+plt.savefig("Corelation heatmap")
+plt.show()
 
 
 g9 = sns.countplot(x="Company", data=great_game, palette=["r", "b", "g"])
